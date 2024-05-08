00eadb79397d4687227f72ecf29c672f300c36e2
AlexanderCecile <alexander.m.cecile@gmail.com>
2024-05-08 13:50:20 -0400

    Initial commit

---
+++ android/app/src/main/java/com/example/w24_app_dev_2_exam_2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.w24_app_dev_2_exam_2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Dev 2 Exam 2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Placeholder(),
+      ),
+    );
+  }
+}
